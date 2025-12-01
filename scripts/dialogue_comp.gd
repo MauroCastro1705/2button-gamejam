129f36ad5487d267b2b9c4b3a8c3885d7580272e
Va1epp <118376724+Va1epp@users.noreply.github.com>
2025-12-01 22:00:08 +0500

    Diaalogue almost finished

--- scripts/dialogue_comp.gd
+++ scripts/dialogue_comp.gd
@@ -9,22 +9,17 @@ var textb: ColorRect
 var bg_node: NinePatchRect
 var sprite_node: NinePatchRect
 var plr: CharacterBody3D
-var batya: dia_area
+var batya: Area3D
 @export var disable: bool = false
 @export var shape: CollisionShape3D
 func set_plr(_plr):
 	plr = _plr
-	name_node = plr.get_node("textbox_root/textb/name")
-	text_node = plr.get_node("textbox_root/textb/text")
-	bg_node = plr.get_node("textbox_root/background")
-	sprite_node = plr.get_node("textbox_root/sprite")
+	name_node = plr.get_node("Character/textbox_root/textb/name")
+	text_node = plr.get_node("Character/textbox_root/textb/text")
+	bg_node = plr.get_node("Character/textbox_root/background")
+	sprite_node = plr.get_node("Character/textbox_root/sprite")
 func show():
-	plr.get_node("SpringArm3D").spring_length = -0.2
-	plr.get_node("SpringArm3D").look_at(get_parent().global_position)
-	plr.get_node("SpringArm3D").rotation_degrees.x = 0
-	plr.get_node("SpringArm3D").rotation_degrees.z = 0
-	plr.go_fpv()
-	plr.get_node("textbox_root").show()
+	plr.get_node("Character/textbox_root").show()
 	name_node.text = dialogue[str_count].namee
 	if dialogue[str_count].sprite != null:
 		sprite_node.texture = dialogue[str_count].sprite
@@ -32,26 +27,18 @@ func show():
 		bg_node.texture = dialogue[str_count].background
 	name_node.text = dialogue[str_count].namee
 	if dialogue[str_count].stop_movement:
-		plr.canmove = false
-		#plr.hide()
-		#plr.get_node("%fps_cam").current = true
-	else:
-		plr.canmove = true
-		#plr.get_node("%main_cam").current = true
-		#plr.show()
+		Global.cam_mode = Global.CamMode.OTS
 	text_node.text = dialogue[str_count].text
 	if after_effs[str_count] != null:
 		after_effs[str_count].use(plr)
 	str_count += 1
 func hide():
-	plr.go_thpv()
 	#plr.show()
-	#plr.get_node("%main_cam").current = true
+	Global.cam_mode = Global.CamMode.SIDE
 	str_count = 0
-	plr.get_node("textbox_root").hide()
+	plr.get_node("Character/textbox_root").hide()
 	name_node.text = ""
 	name_node.name = ""
-	plr.canmove = true
 	if disable:
 		batya.can_int = false
 		if batya.get_node("CollisionShape3D") != null:
