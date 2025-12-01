7a9d1bbf6b586a86080b75e89ca7c24448b2395e
Mauro Castro <maurocastruli@gmail.com>
2025-12-01 11:21:48 -0300

    player movement rework

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,31 +3,41 @@ extends CharacterBody3D
 @export_subgroup("Components")
 @export var view: Node3D
 
-@export_subgroup("Properties")
-@export var movement_speed := 250.0
-@export var jump_strength := 7.0
-
 var movement_velocity: Vector3
 
 @onready var model := $Character
 @onready var animation := $Character/AnimationPlayer
+@export var move_speed: float = 3.0
+@export var turn_speed_deg: float = 180.0   # degrees per second when held
+@export var snap_turn_deg: float = 0.0      # set (e.g. 45) to use discrete snap turns on tap; 0 = smooth turning
 
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y -= jump_strength
 	handle_controls(delta)
 	model.scale = model.scale.lerp(Vector3.ONE, delta * 10.0)
+	
+
 func handle_controls(delta: float) -> void:
 	# Only allow movement if the global camera mode is SIDE
-	if Global.cam_mode == Global.CamMode.SIDE:
-		var input_dir = Input.get_vector("1", "2", "4", "3")
-		input_dir.y = -1
-		var direction = (transform.basis * Vector3(-input_dir.x, 0, input_dir.y))
-		velocity.x = direction.x * movement_speed
-		velocity.z = direction.z * movement_speed
-		move_and_slide()
-		if Input.is_action_pressed("button_one"):
-			rotation_degrees.y += 1
-		if Input.is_action_pressed("button_two"):
-			rotation_degrees.y -= 1
+	if Global.cam_mode != Global.CamMode.SIDE:
+		return
+	# --- Turning ---
+	## Two modes: smooth (hold to rotate) or snap (press to rotate by fixed angle)
+	if snap_turn_deg > 0.0:
+		if Input.is_action_just_pressed("button_two"):
+			rotate_y(-deg_to_rad(snap_turn_deg))  # left
+		if Input.is_action_just_pressed("button_one"):
+			rotate_y( deg_to_rad(snap_turn_deg))  # right
+	else:
+		var turn_dir := int(Input.is_action_pressed("button_one")) - int(Input.is_action_pressed("button_two"))
+		if turn_dir != 0:
+			rotate_y(deg_to_rad(turn_speed_deg) * turn_dir * delta)
+
+	var forward := -transform.basis.z
+	forward.y = 0.0
+	forward = forward.normalized()
+
+	var target_vel := forward * move_speed
+	velocity.x = target_vel.x
+	velocity.z = target_vel.z
+	move_and_slide()
