36541d2c36e7e7b3c96d1b37116e2839ab12ada5
Va1epp <118376724+Va1epp@users.noreply.github.com>
2025-12-01 21:43:32 +0500

    dialogue imported

---
+++ scripts/dialogue.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name dialogue_string
+@export var namee: String
+@export var text: String
+@export var stop_movement: bool
+@export var background: Texture2D
+@export var sprite: Texture2D
---
+++ scripts/dialogue_area.gd
@@ -0,0 +1,20 @@
+extends Area3D
+class_name dia_area
+@export var dia: dia_comp
+var can_int: bool = false
+func _ready() -> void:
+	connect("body_entered", plr_enter)
+	connect("body_exited", plr_exit)
+func plr_enter(body):
+	if body.is_in_group("player"):
+		can_int = true
+		dia.set_plr(body)
+		dia.batya = self
+func plr_exit(body):
+	if body.is_in_group("player"):
+		dia.hide()
+		can_int = false
+		dia.plr = null
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact") and can_int:
+		dia.check_what_to_do()
