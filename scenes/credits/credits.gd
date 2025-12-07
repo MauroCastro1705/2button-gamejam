c81c7c0864ca975d4302e2213e62c762c4930926
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2025-12-07 14:31:54 -0300

    credits scene

--- scenes/credits/credits.gd
+++ scenes/credits/credits.gd
@@ -2,9 +2,39 @@ extends Node2D
 const MAIN_MENU := "res://scenes/menu/main_menu.tscn"
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("button_one"):
-		go_back()
+
 
 func go_back():
 	get_tree().change_scene_to_file(MAIN_MENU)
+
+
+@onready var deploy_bar: ProgressBar = %DeployBar
+const HOLD_ACTION := "button_one"
+const HOLD_TIME := 3.0
+
+var hold_time: float = 0.0
+var scene_changing: bool = false
+
+
+func _ready() -> void:
+	deploy_bar.value = 0
+	if deploy_bar.max_value <= 0:
+		deploy_bar.max_value = 100
+
+
+func _process(delta: float) -> void:
+	if scene_changing:
+		return
+
+	if Input.is_action_pressed(HOLD_ACTION):
+		hold_time += delta
+	else:
+		if hold_time > 0.0:
+			hold_time = 0.0
+
+	var progress = clamp(hold_time / HOLD_TIME, 0.0, 1.0)
+	deploy_bar.value = progress * deploy_bar.max_value
+
+	if hold_time >= HOLD_TIME:
+		scene_changing = true
+		go_back()
